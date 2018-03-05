f6602634527b9ef88119391cf266bd809d2ab0a8
Yaro Shkvorets <shkvorets@gmail.com>
2018-03-05 11:24:37 -0500

    Update start-osrm-server.sh

--- start-osrm-server.sh
+++ start-osrm-server.sh
@@ -1,11 +1,11 @@
 OSRM=./node_modules/osrm/lib/binding/osrm-routed
 
 if ! [ -x "$(command -v $OSRM)" ]; then
-  echo "Error: Missing OSRM. Install using 'npm install osrm@5.15.1'\n"
+  echo "Error: Missing OSRM. Install all dependencies using 'npm install'\n"
   exit 1
 fi
 
-./node_modules/osrm/lib/binding/osrm-routed -p 5001 data/lts1/data.osrm &
-./node_modules/osrm/lib/binding/osrm-routed -p 5002 data/lts2/data.osrm &
-./node_modules/osrm/lib/binding/osrm-routed -p 5003 data/lts3/data.osrm &
-./node_modules/osrm/lib/binding/osrm-routed -p 5004 data/lts4/data.osrm &
+$OSRM -p 5001 data/lts1/data.osrm &
+$OSRM -p 5002 data/lts2/data.osrm &
+$OSRM -p 5003 data/lts3/data.osrm &
+$OSRM -p 5004 data/lts4/data.osrm &
