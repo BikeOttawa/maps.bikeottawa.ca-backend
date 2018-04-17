747a2d1e32eb4ff0bbc0d4ed5c4889d0cf2bf10d
zzptichka <shkvorets@gmail.com>
2018-04-16 22:42:27 -0400

    changed restart-maps to work from crontab

--- restart-maps.sh
+++ restart-maps.sh
@@ -1,4 +1,10 @@
+script_dir=$(dirname $(readlink -f $0) )
+echo script_dir is $script_dir
+cd $script_dir
+
 OSRM=./node_modules/osrm/lib/binding/osrm-routed
+
+echo "expecting OSRM to be in $OSRM"
 
 if ! [ -x "$(command -v $OSRM)" ]; then
   echo "Error: Missing OSRM. Install using 'npm install osrm@5.15.1'\n"
@@ -14,4 +20,4 @@ $OSRM -p 5002 data/lts2/data.osrm &
 $OSRM -p 5003 data/lts3/data.osrm &
 $OSRM -p 5004 data/lts4/data.osrm &
 
-node iso-server.js & 
+node iso-server.js &
